88fe61440b3c79124e77e507fe7811c8ed03c966
milescajus <80205080+milescajus@users.noreply.github.com>
2021-10-05 14:55:06 -0500

    new and improved  routine

--- Pathfinder.cs
+++ Pathfinder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Pathfinder
 {
@@ -12,7 +13,7 @@ class Pathfinder
 		width = 10;
 		height = 12;
 		grid = new bool[height, width];
-		m_points = 6;
+		m_points = Int32.Parse(args[0]);
 		origin = new int[] {4, 5};
 		
 		
@@ -25,18 +26,19 @@ class Pathfinder
 						  {2, 9}, {3, 9}, {4, 9}, {5, 9}, {6, 9}, {7, 9}};
 						  
 		InitGrid(blocked);
+        Console.WriteLine("Initial state:");
 		PrintGrid();
+        Console.WriteLine();
         /*
         for (int i = 0; i < grid.GetLength(0); i++) {
             for (int j = 0; i < grid.GetLength(1); i++) {
                 origin = new int[] {4, 5};
                 Move(new int[] {i, j});
-                PrintGrid();
             }
         }
         */
 
-        int[] dest = new int[] {1, 1};
+        int[] dest = new int[] {4, 7};
         Move(dest);
 	}
 	
@@ -46,12 +48,11 @@ class Pathfinder
 				if (i == origin[1] && j == origin[0] /*i == 5 && j == 4*/) {
 					Console.Write("[X]");
 				} else {
-					Console.Write(grid[i, j] ? "[ ]" : "[-]");
+					Console.Write(grid[i, j] ? "[ ]" : "[*]");
 				}
 			}
 			Console.WriteLine();
 		}
-        Console.WriteLine();
 	}
 	
 	public static void InitGrid(int[,] blocked) {
@@ -67,33 +68,48 @@ class Pathfinder
 	}
 	
 	public static void Move(int[] dest) {
-        int[] best_nb = origin;
+        while (m_points > 0 && !(origin[0] == dest[0] && origin[1] == dest[1])) {
+            // has movement points and not at destination
+            var nbs = new Dictionary<double, int[]>();
+            var dists = new List<double>();
 
-        while (m_points > 0) {
 		    foreach (int[] nb in FindNeighbors()) {
+
 			    if (grid[nb[1], nb[0]]) { // free neighbor exists
-                    double cur_dist = Math.Pow(dest[0] - nb[0], 2) + Math.Pow(dest[1] - nb[1], 2);
-                    double best_dist = Math.Pow(dest[0] - best_nb[0], 2) + Math.Pow(dest[1] - best_nb[1], 2);
-				    if (cur_dist < best_dist) { // squared distance of neighbor is less than current position
-                        best_nb = nb;
-					    break;
-			        } else { continue; } // look for better neighbors
+                    double distance = Math.Pow(dest[0] - nb[0], 2) + Math.Pow(dest[1] - nb[1], 2);
+
+                    if (nbs.ContainsKey(distance)) {
+                        // if move already exists with same distance, flip a coin to choose
+                        var rnd = new Random();
+                        nbs[distance] = rnd.Next(2) == 0 ? nbs[distance] : nb;
+                    } else {
+                        nbs.Add(distance, nb);
+                    }
                 }
 		    }
-			origin = best_nb;
-			m_points--;
-            Console.WriteLine("Remaining moves: " + m_points);
+
+            // Sort neighbor distances
+            foreach (double d in nbs.Keys) {
+                dists.Add(d);
+            }
+
+            dists.Sort();
+
+            // TODO: find alternative to creating walls for preventing getting caught in a loop
+            grid[origin[1], origin[0]] = false; // block previous path
+            origin = nbs[dists[0]];             // move to neighbor closest to destination
+            m_points--;
+
             PrintGrid();
+            Console.WriteLine("Remaining moves: " + m_points + "\n");
         }
 	}
 	
 	public static int[][] FindNeighbors() {
-		int[] north = new int[] {origin[1] - 1, origin[0]};
-		int[] south = new int[] {origin[1] + 1, origin[0]};
-		int[] east = new int[] {origin[1], origin[0] + 1};
-		int[] west = new int[] {origin[1], origin[0] - 1};
-		
-		
+		int[] north = new int[] {origin[0], origin[1] - 1};
+		int[] south = new int[] {origin[0], origin[1] + 1};
+		int[] east = new int[] {origin[0] + 1, origin[1]};
+		int[] west = new int[] {origin[0] - 1, origin[1]};
 		
 		return new int[][] {north, south, east, west};
 	}
