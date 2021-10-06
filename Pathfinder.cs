f118251948757937ebbfca2cd01177dd6e8207d0
milescajus <80205080+milescajus@users.noreply.github.com>
2021-10-06 16:36:31 -0500

    fixed indentation

--- Pathfinder.cs
+++ Pathfinder.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 
 class Pathfinder
 {
-	static int width;
-	static int height;
-	static bool[,] grid;
-	static int[] origin;
-	static int m_points;
-	
-	public static void Main(string[] args) {
-		width = 10;
-		height = 12;
-		grid = new bool[height, width];
-		m_points = Int32.Parse(args[0]);
-		origin = new int[] {4, 5};
-		
-		
-		int[,] blocked = {{2, 2}, {5, 2}, {6, 2},
-						  {2, 3}, {6, 3},
-						  {2, 4},
-						  {6, 5},
-						  {2, 6}, {3, 6}, {4, 6}, {5, 6}, {6, 6}, {7, 6},
-						  {2, 8},
-						  {2, 9}, {3, 9}, {4, 9}, {5, 9}, {6, 9}, {7, 9}};
-						  
-		InitGrid(blocked);
+    static int width;
+    static int height;
+    static bool[,] grid;
+    static int[] origin;
+    static int m_points;
+    
+    public static void Main(string[] args) {
+        width = 10;
+        height = 12;
+        grid = new bool[height, width];
+        m_points = Int32.Parse(args[0]);
+        origin = new int[] {4, 5};
+        
+        
+        int[,] blocked = {{2, 2}, {5, 2}, {6, 2},
+                          {2, 3}, {6, 3},
+                          {2, 4},
+                          {6, 5},
+                          {2, 6}, {3, 6}, {4, 6}, {5, 6}, {6, 6}, {7, 6},
+                          {2, 8},
+                          {2, 9}, {3, 9}, {4, 9}, {5, 9}, {6, 9}, {7, 9}};
+                          
+        InitGrid(blocked);
         Console.WriteLine("Initial state:");
-		PrintGrid();
+        PrintGrid();
         Console.WriteLine();
         /*
         for (int i = 0; i < grid.GetLength(0); i++) {
@@ -40,42 +40,42 @@ class Pathfinder
 
         int[] dest = new int[] {4, 7};
         Move(dest);
-	}
-	
-	public static void PrintGrid() {
-		for (int i = 0; i < grid.GetLength(0); i++) {
-			for (int j = 0; j < grid.GetLength(1); j++) {
-				if (i == origin[1] && j == origin[0] /*i == 5 && j == 4*/) {
-					Console.Write("[X]");
-				} else {
-					Console.Write(grid[i, j] ? "[ ]" : "[*]");
-				}
-			}
-			Console.WriteLine();
-		}
-	}
-	
-	public static void InitGrid(int[,] blocked) {
-		for (int i = 0; i < grid.GetLength(0); i++) {
-			for (int j = 0; j < grid.GetLength(1); j++) {
-				grid[i, j] = true;
-			}
-		}
-		
-		for (int i = 0; i < blocked.GetLength(0); i++) {
-			grid[blocked[i, 1], blocked[i, 0]] = false;
-		}
-	}
-	
-	public static void Move(int[] dest) {
+    }
+    
+    public static void PrintGrid() {
+        for (int i = 0; i < grid.GetLength(0); i++) {
+            for (int j = 0; j < grid.GetLength(1); j++) {
+                if (i == origin[1] && j == origin[0] /*i == 5 && j == 4*/) {
+                    Console.Write("[X]");
+                } else {
+                    Console.Write(grid[i, j] ? "[ ]" : "[*]");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public static void InitGrid(int[,] blocked) {
+        for (int i = 0; i < grid.GetLength(0); i++) {
+            for (int j = 0; j < grid.GetLength(1); j++) {
+                grid[i, j] = true;
+            }
+        }
+        
+        for (int i = 0; i < blocked.GetLength(0); i++) {
+            grid[blocked[i, 1], blocked[i, 0]] = false;
+        }
+    }
+    
+    public static void Move(int[] dest) {
         while (m_points > 0 && !(origin[0] == dest[0] && origin[1] == dest[1])) {
             // has movement points and not at destination
             var nbs = new Dictionary<double, int[]>();
             var dists = new List<double>();
 
-		    foreach (int[] nb in FindNeighbors()) {
+            foreach (int[] nb in FindNeighbors()) {
 
-			    if (grid[nb[1], nb[0]]) { // free neighbor exists
+                if (grid[nb[1], nb[0]]) { // free neighbor exists
                     double distance = Math.Pow(dest[0] - nb[0], 2) + Math.Pow(dest[1] - nb[1], 2);
 
                     if (nbs.ContainsKey(distance)) {
@@ -86,7 +86,7 @@ class Pathfinder
                         nbs.Add(distance, nb);
                     }
                 }
-		    }
+            }
 
             // Sort neighbor distances
             foreach (double d in nbs.Keys) {
@@ -103,14 +103,14 @@ class Pathfinder
             PrintGrid();
             Console.WriteLine("Remaining moves: " + m_points + "\n");
         }
-	}
-	
-	public static int[][] FindNeighbors() {
-		int[] north = new int[] {origin[0], origin[1] - 1};
-		int[] south = new int[] {origin[0], origin[1] + 1};
-		int[] east = new int[] {origin[0] + 1, origin[1]};
-		int[] west = new int[] {origin[0] - 1, origin[1]};
-		
-		return new int[][] {north, south, east, west};
-	}
+    }
+    
+    public static int[][] FindNeighbors() {
+        int[] north = new int[] {origin[0], origin[1] - 1};
+        int[] south = new int[] {origin[0], origin[1] + 1};
+        int[] east = new int[] {origin[0] + 1, origin[1]};
+        int[] west = new int[] {origin[0] - 1, origin[1]};
+        
+        return new int[][] {north, south, east, west};
+    }
 }
